57d08cec5d844986ed59049467379cdfd0863c73
steph-nnamani@gmail.com <steph-nnamani@gmail.com>
2025-03-21 21:48:47 -0500

    First commit

--- aws_infra_pipeline/aws_infra_pipeline_stack.py
+++ aws_infra_pipeline/aws_infra_pipeline_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_s3 as s3,
+    RemovalPolicy
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -8,12 +10,14 @@ from constructs import Construct
 class AwsInfraPipelineStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+        super().__init__(scope, construct_id, **kwargs)    
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsInfraPipelineQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create S3 bucket with standard configuration
+        bucket = s3.Bucket(self, "Bucket",
+            bucket_name="zaralinkawspipelinestore",
+            versioned=True,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+            removal_policy=RemovalPolicy.DESTROY
+        )        
+                
